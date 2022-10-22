550754ba37d39d4e49e9e843af354f842f959b38
nikhea <imonikheaugbodaga@gmail.com>
2022-10-22 09:18:52 +0100

    created newsletter component

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import "./App.css";
 import Header from "./components/Header";
 import FilterCard from "./components/card/filterCard";
 import Carousel from "./components/caroules/carousel";
-
+import NewsLetter from "./components/Newsletter/newsletter";
 const style = {
-  app:`relative `
-}
+  app: `relative `,
+};
 const App: FC = () => {
   return (
     <div className={style.app}>
       <Header />
       <FilterCard />
       <Carousel />
+      <NewsLetter />
     </div>
   );
 };
