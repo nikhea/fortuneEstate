f6b66e4c9599f07e95749858895b398f9542ce04
nikhea <imonikheaugbodaga@gmail.com>
2022-10-22 15:46:43 +0100

    responsive/maintext

--- src/components/Header/MainText.tsx
+++ src/components/Header/MainText.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 
 const style = {
-  container: `flex  w-[95%]  m-auto flex-col items-center`,
-  title: `text-[5.5rem] font-light`,
-  subTitle: `text-8xl font-light font-bold`,
+  container: `flex  w-[95%]  m-auto flex-col items-center justify-center h-[29em] text-center `,
+  title: `text-[3rem] md:text-[5.5rem] font-light`,
+  subTitle: `text-5xl md:text-8xl font-light font-bold`,
   text: `text-lg font-light my-2 tracking-widest`,
   btn:`uppercase bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full px-9 py-2 tracking-widest mt-5 `
 };
