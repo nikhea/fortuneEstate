e534d50bc93e4fde972f37750e94e8895ae43e7f
nikhea <imonikheaugbodaga@gmail.com>
2022-10-22 11:24:58 +0100

    design newsletter

--- src/components/Newsletter/newsletter.tsx
+++ src/components/Newsletter/newsletter.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { FC } from "react";
 
-const newsletter = () => {
+const style = {
+  container: `text-white  w-[85%] m-auto flex justify-between pt-[4.5%] pr-[0%] pb-[4%] pl-[0%]`,
+  bgContainer: `bg-[#2EA8F2]`,
+  textContainer: ``,
+  title: `text-3xl leading-[2em] `,
+  subText: `text-base`,
+  inputContainer: `relative w-[450px] max-w-[96%] mt-[2em]  `,
+  emailInput:`w-full outline-none text-black rounded-[30px] py-[15px] px-[32px] bg-[rgba(255,255,255,0.3)] text-[.9375rem] shadow-2xl shadow-[0 3px 35px #00000040]`,
+  btn:`my-[4px] mx-[5px] py-[0] px-[25px] h-[44px] text-[1rem]  rounded-full z-2 right-0 absolute outline-none  bg-gradient-to-r from-cyan-500 to-blue-500`
+};
+// 0 3px 35px #00000040
+const newsletter: FC = () => {
   return (
-    <div>newsletter</div>
-  )
-}
+    <div className={style.bgContainer}>
+      <div className={style.container}>
+        <div className={style.textContainer}>
+          <h2 className={style.title}>Sign Up For Our Daily Newsletter</h2>
+          <p className={style.subText}>
+            Sign up for our emails and be the first to know about special
+            offers.
+          </p>
+        </div>
+        <form className={style.inputContainer}>
+          <input className={style.emailInput} type="email" placeholder="Enter Your Email" />
+          <input className={style.btn} type="submit" value="SUBMIT" />
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default newsletter
+export default newsletter;
