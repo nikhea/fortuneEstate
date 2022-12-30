6fcc5a3132993f05640eae4bfec883dd6c51892e
nikhea <imonikheaugbodaga@gmail.com>
2022-12-30 12:31:16 +0100

    added faq style

--- src/pages/contact/components/faq/index.tsx
+++ src/pages/contact/components/faq/index.tsx
@@ -2,17 +2,17 @@ import React, { useEffect, useState } from "react";
 import Faq from "react-faq-component";
 
 const style = {
-  container: `items-center justify-center grid gap-4 grid-cols-1 md:grid-cols-2`,
-  title: ` uppercase text-[1.5rem] text-[#7f7f7f] font-normal tracking-[1.1px] text-center`,
-  textContainer: ``,
-  tage: `uppercase `,
+  container: `items-center text-center justify-center grid gap-4 grid-cols-1 md:grid-cols-2`,
+  title: ` uppercase text-5xl text-[#7f7f7f] font-normal tracking-[1.1px] text-center font-[900]`,
+  textContainer: `relative`,
+  tage: `uppercase text-3xl text-[#7f7f7f]  my-2`,
   discoverTitle: `text-[#7f7f7f] capitalize font-bold text-5xl mb-[1em]`,
 };
 const index = () => {
   return (
     <div className={style.container}>
       <div className={style.textContainer}>
-        <div className={style.tage}>faq</div>
+        {/* <div className={style.tage}>faq</div> */}
         <h1 className={style.title}>freqently asked questions.</h1>
       </div>
       <Faq data={data} styles={styles} config={config} />;
@@ -58,8 +58,11 @@ const data = {
               Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
     },
     {
-      title: "What is the package version",
-      content: <p>current version is 1.2.1</p>,
+      title: "Lorem ipsum dolor sit amet,",
+      content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
+              Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
+              Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
+              Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
     },
   ],
 };
