4d38019f17e113223360c611ece23d4c874b2365
nikhea <imonikheaugbodaga@gmail.com>
2023-08-30 13:11:11 +0100

    added background images for contact banner and advert banner

--- src/components/AdvertBanner/index.tsx
+++ src/components/AdvertBanner/index.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 const style = {
-  bgImage: `  md:mt-[2em] w-full h-[60vh] md:h-[340px]  bg-cover bg-center bg-no-repeat relative bg-fixed object-content z-50 bg-[url('https://www.thehouse48.com/wp-content/uploads/2021/12/pexels-photo-396303.jpeg')]`,
+  bgImage: `  md:mt-[2em] w-full h-[60vh] md:h-[440px]  bg-cover bg-center bg-no-repeat relative bg-fixed object-content z-50 bg-[url('https://res.cloudinary.com/djkqaqoj3/image/upload/v1693397026/photo-1683721003111-070bcc053d8b_aro2zx.jpg')]`,
   container: `h-full  w-[85%] m-auto flex flex-col md:flex-row  justify-center md:justify-between md:items-center`,
   textContainer: `text-white z-50   md:text-start mt-5 text-center w-full`,
   title: `text-3xl md:text-7xl font-black uppercase tracking-wider text-center`,
   overlay: `h-full w-full top-0 left-0 absolute bg-black opacity-60 bg-fixed -z-50`,
 };
-
+// https://res.cloudinary.com/djkqaqoj3/image/upload/v1693397026/photo-1683721003111-070bcc053d8b_aro2zx.jpg
+// https://www.thehouse48.com/wp-content/uploads/2021/12/pexels-photo-396303.jpeg
 const contactBanner: FC = () => {
   return (
     <div className={style.bgImage}>
