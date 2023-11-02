fd7f09c33bf038029df869c0ac744d353f5d24ec
nikhea <imonikheaugbodaga@gmail.com>
2023-11-02 09:40:15 +0100

    added typeSafty for form

--- src/pages/dashBoard/pages/uploads/components/form/propertySubmitSummary/propertySummary.tsx
+++ src/pages/dashBoard/pages/uploads/components/form/propertySubmitSummary/propertySummary.tsx
@@ -1,8 +1,15 @@
+import { FormData } from "../../formInterface";
 const PropertySummary: React.FC = () => {
-  const summary = JSON.parse(localStorage.getItem("storageKey") || "{}");
+  const summary: FormData = JSON.parse(
+    localStorage.getItem("storageKey") || "{}"
+  );
   console.log("summary", (summary as any).category);
 
-  return <div>propertySummary</div>;
+  return (
+    <div>
+      <h1>property title : {summary.title}</h1>
+    </div>
+  );
 };
 
 export default PropertySummary;
