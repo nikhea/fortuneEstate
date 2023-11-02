399d577b5108296b60108a62d57ce4ac887690bc
nikhea <imonikheaugbodaga@gmail.com>
2023-11-02 09:22:08 +0100

    getting property details into summary component

--- src/pages/dashBoard/pages/uploads/components/form/propertySubmitSummary/propertySummary.tsx
+++ src/pages/dashBoard/pages/uploads/components/form/propertySubmitSummary/propertySummary.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 const PropertySummary = () => {
+  const getLocalStorage = () => {
+    if (localStorage.getItem("data")) {
+      let localStorageItem = JSON.parse(localStorage.getItem("data"));
+    }
+  };
   return <div>propertySummary</div>;
 };
 
