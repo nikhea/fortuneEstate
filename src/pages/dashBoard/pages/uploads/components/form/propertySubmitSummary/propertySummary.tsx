fa4d2176db702cd15dd95739dd66eb060a0638e1
nikhea <imonikheaugbodaga@gmail.com>
2023-11-02 09:49:01 +0100

    adding details

--- src/pages/dashBoard/pages/uploads/components/form/propertySubmitSummary/propertySummary.tsx
+++ src/pages/dashBoard/pages/uploads/components/form/propertySubmitSummary/propertySummary.tsx
@@ -1,13 +1,16 @@
 import { FormData } from "../../formInterface";
 const PropertySummary: React.FC = () => {
-  const { category, title, priceSymbol }: FormData = JSON.parse(
+  const { category, title, priceSymbol, price }: FormData = JSON.parse(
     localStorage.getItem("storageKey") || "{}"
   );
   console.log("summary");
 
   return (
-    <div>
-      <h1>property title : {priceSymbol}</h1>
+    <div className=" m-auto mx-20 mb-10 uppercase">
+      <h1 className="  ">property title : {title}</h1>
+      <p>
+        price : {price} {priceSymbol}
+      </p>
     </div>
   );
 };
