838a2ef8917d211e6586e8414592170374eee7a0
nikhea <imonikheaugbodaga@gmail.com>
2024-03-02 04:35:07 +0100

    Wip: chaged app color theme

--- src/Layout/Newsletter/newsletter.tsx
+++ src/Layout/Newsletter/newsletter.tsx
@@ -3,12 +3,12 @@ import Button from "../../components/UI/FormElement/Button";
 
 // bg-[#e6e9efa3]
 const style = {
-  discoverContainer: `capitalize text-center w-[92%] lg:w-[50%] m-auto  h-[40vh] bg-[#0D304A] text-white my-[7%] rounded-[30px]`,
-  container: ` flex items-center justify-center  flex-col  h-full`,
+  discoverContainer: `capitalize text-center w-[92%] lg:w-[50%] m-auto h-[35vh] md:h-[80vh]  lg:h-[40vh] bg-[#0D304A] text-white my-[7%] rounded-[30px]`,
+  container: ` flex items-center justify-center  flex-col  h-full  px-4`,
   bgContainer: `bg-[#E5E5E5] `,
   textContainer: ``,
-  title: `text-3xl leading-[2em] `,
-  subText: ` text-sm text-base lg:tracking-`,
+  title: `text-[18px]  lg:text-3xl leading-[2em] `,
+  subText: ` text-sm md:text-base lg:tracking-`,
   inputContainer: ` w-[450px] max-w-[96%] mt-[2em] flex flex-col justify-end items-end `,
   emailInput: `w-full outline-none text-black rounded-[30px] py-[15px] px-[32px] outline-none text-[#111]  text-[.9375rem] `,
   btn: ` bg-white my-3 px-[22px] py-[10px] text-[#0D304A] uppercase rounded-[30px]`,
